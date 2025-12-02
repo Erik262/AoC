fdc6efaeb22097dacc36e4d15084b782c9251c7b
Erik Traise <erik.traise@gmail.com>
2025-12-02 13:04:46 +0100

    feat: finalize day2

--- day_02/puzzle.py
+++ day_02/puzzle.py
@@ -6,16 +6,39 @@ def check(num: int) -> bool:
 
     return str_num[:half] == str_num[half:]
 
+def check_two(num: int) -> bool:
+    str_num = str(num)
+
+    for i in range(1, len(str_num) // 2 + 1):
+        head = str_num[:i]
+        tail = str_num[i: i + len(head)]
+        # print(f"head: {head}, tail: {tail}")
+        
+        if head == tail:
+            are_same = True
+
+            if (len(str_num) % len(head)) != 0:
+                continue
+
+            for j in range(1, len(str_num) // len(head)):
+                mov_tail = str_num[i*j: (i + len(head)*j)]
+                # print(f"head: {head}, mov_tail: {mov_tail}")
+                are_same = are_same and (head == mov_tail)
+
+            return are_same
+    return False
+
 def product(start: int, end: int) -> list:
     results = []
+
     for n in range(start, end + 1):
-        if check(n):
+        if check(n) or check_two(n):
             results.append(n)
     return results
 
 def main():
-    input_path = pathlib.Path(__file__).parent / "test.txt"
-    # input_path = pathlib.Path(__file__).parent / "input.txt"
+    # input_path = pathlib.Path(__file__).parent / "test.txt"
+    input_path = pathlib.Path(__file__).parent / "input.txt"
     ids = input_path.read_text(encoding='utf-8').split(',') 
 
     results = []
@@ -28,8 +51,8 @@ def main():
         results.append(res)
         added_up += sum(res)
 
+    # print(results)
     print(added_up)
-
 
 if __name__ == "__main__":
     main()
