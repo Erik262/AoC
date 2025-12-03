2bcd62f3d239ada3c1a7ae8ca75cf073b1e2db1c
Erik Traise <erik.traise@gmail.com>
2025-12-03 17:08:23 +0100

    feat: finalize part 2

--- day_03/puzzle.py
+++ day_03/puzzle.py
@@ -3,7 +3,6 @@ from collections import defaultdict, Counter
 import heapq
 from typing import List
 
-
 def get_joltage(bank: str) -> int:
     l, r = 0, 1
     joltage = 0
@@ -25,6 +24,27 @@ def get_joltage(bank: str) -> int:
 
     return joltage
 
+def get_joltage2(bank: str) -> int:
+    n = len(bank)
+    k = 12
+
+    if n <= k:
+        return int(bank)
+    
+    to_remove = n - k
+    stack = []
+
+    for batt in bank:
+        while to_remove > 0 and stack and stack[-1] < batt:
+            stack.pop()
+            to_remove -= 1
+
+        stack.append(batt)
+
+    if to_remove > 0:
+        stack = stack[:-to_remove]
+
+    return int("".join(stack))
 
 def main():
     input_path = pathlib.Path(__file__).parent / "input.txt"
@@ -33,7 +53,8 @@ def main():
 
     results = []
     for bank in banks:
-        results.append(get_joltage(bank))
+        # results.append(get_joltage(bank))
+        results.append(get_joltage2(bank))
 
     # print(results)
     print(sum(results))
