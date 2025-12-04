54ba9c930e7803d98b14947bb80c0f12c36587e8
Erik Traise <erik.traise@gmail.com>
2025-12-04 15:51:46 +0100

    feat: finalize puzzle 2

--- day_04/puzzle.py
+++ day_04/puzzle.py
@@ -1,10 +1,29 @@
 import pathlib
-from collections import defaultdict, Counter
-import heapq
-from typing import List
 
 import torch
 import torch.nn.functional as F
+
+KERNEL = torch.tensor( # (N=1, C=1, H=3, W=3)
+    [[1.,1.,1.],
+        [1.,0.,1.],
+        [1.,1.,1.]]
+    ).unsqueeze(0).unsqueeze(0)
+
+def step(grid: torch.Tensor) -> tuple[torch.Tensor, bool, int]:
+    g = grid.unsqueeze(0).unsqueeze(0) # (N=1, C=1, H, W)
+    n_count = F.conv2d(g, KERNEL, padding=1).squeeze()
+
+    center = (grid == 1)
+    mask = center & (n_count < 4)
+    num_removed = int(mask.sum().item())
+
+    if num_removed == 0:
+        return grid, False, 0
+    
+    new_grid = grid.clone()
+    new_grid[mask] = 0
+
+    return new_grid, True, num_removed
 
 def main():
     input_path = pathlib.Path(__file__).parent / "input.txt"
@@ -13,27 +32,20 @@ def main():
 
     grid_replace = [[1 if c == '@' else 0 for c in row] for row in lines]
     grid = torch.tensor(grid_replace, dtype=torch.float32)
-    H, W = grid.shape
+    # H, W = grid.shape
 
-    grid = grid.unsqueeze(0).unsqueeze(0) # (N=1, C=1, H, W)
+    total_removed = 0
 
-    kernel = torch.tensor(
-        [[1.,1.,1.],
-         [1.,0.,1.],
-         [1.,1.,1.]]
-        )
-    
-    kernel = kernel.unsqueeze(0).unsqueeze(0) # (N=1, C=1, H=3, W=3)
-    n_count = F.conv2d(grid, kernel, padding=1).squeeze()
+    while True:
+        grid, changed, num_removed = step(grid)
+        total_removed += num_removed
+        
+        if not changed:
+            break
 
-    mask = n_count < 4
-    num_pos = mask.sum().item()
+    # final_ones = int(grid.sum().item())
 
-    center = (grid == 1)
-
-    mask_active = center & mask
-    num_active = mask_active.sum().item()
-    print(num_active)
+    print(total_removed)
 
 if __name__ == "__main__":
     main()
