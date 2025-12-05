44fb8923006854ef781aafd7d6cd15533fcbed86
Erik Traise <erik.traise@gmail.com>
2025-12-05 17:57:42 +0100

    feat: finalize part 2

--- day_05/puzzle.py
+++ day_05/puzzle.py
@@ -2,12 +2,33 @@ import pathlib
 
 def check(id: int, fresh_range) -> bool:
     for rang in fresh_range:
-        l, r = rang.split('-')
+        l, r = map(int, rang.split('-'))
             
-        if id in range(int(l), int(r) + 1):
+        if id in range(l, r + 1):
             return True
     
     return False
+
+def check2(fresh_range) -> int:
+    intervals = []
+
+    for rang in fresh_range:
+        l, r = map(int, rang.split('-'))
+        intervals.append((l, r))
+
+    intervals.sort()
+    total = 0
+    cur_l, cur_r = intervals[0]
+
+    for l, r in intervals[1:]:
+        if l <= cur_r + 1:
+            cur_r = max(cur_r, r)
+        else:
+            total += cur_r - cur_l + 1
+            cur_l, cur_r = l, r
+
+    total += cur_r - cur_l + 1
+    return total
 
 def main():
     input_path = pathlib.Path(__file__).parent / "input.txt"
@@ -24,6 +45,7 @@ def main():
             fresh_counter += 1
 
     print(fresh_counter)
+    print(check2(fresh_range))
 
 if __name__ == "__main__":
     main()
